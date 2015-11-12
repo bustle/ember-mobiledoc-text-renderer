ff0aa1d18d25c9e9cfdfebb54ebe709a63b14720
Cory Forsyth <cory.forsyth@gmail.com>
2015-11-12 15:56:20 -0500

    v0.1.0

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-mobiledoc-text-renderer'
+  name: 'ember-mobiledoc-text-renderer',
+  included: function() {
+    var libRoot = require.resolve('mobiledoc-text-renderer/lib');
+    var libPath = path.dirname(libRoot);
+
+    this.treePaths.addon = libPath;
+  }
 };
